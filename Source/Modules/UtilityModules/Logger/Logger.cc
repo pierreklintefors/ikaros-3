8ac6a5c7023dbc4f5aa99d36c85268cee2e9d3a2
Christian Balkenius <christian@ikaros-project.org>
2023-05-21 17:16:07 +0200

    Update Logger.cc

--- Source/Modules/UtilityModules/Logger/Logger.cc
+++ Source/Modules/UtilityModules/Logger/Logger.cc
@@ -9,10 +9,10 @@ public:
 
     void Init()
     {
-    Socket socket;
-    char b[2048];
-    socket.Get("www.ikaros-project.org", 80, "GET /start3/ HTTP/1.1\r\nHost: www.ikaros-project.org\r\nConnection: close\r\n\r\n", b, 1024);
-    socket.Close();
+        Socket socket;
+        char b[2048];
+        socket.Get("www.ikaros-project.org", 80, "GET /start3/ HTTP/1.1\r\nHost: www.ikaros-project.org\r\nConnection: close\r\n\r\n", b, 1024);
+        socket.Close();
     }
 };
 
