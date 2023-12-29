0cd31f747dcb6c5bccdb54c13aad875f3da9b8da
Christian Balkenius <christian@ikaros-project.org>
2023-12-29 16:35:57 +0100

    image file formats first commit

---
+++ Source/Kernel/image_file_formats.h
@@ -0,0 +1,21 @@
+// image_file_format.h
+// Copyright (C) 2023  Christian Balkenius
+
+#ifndef IMAGE_FILE_FORMAT
+#define IMAGE_FILE_FORMAT
+
+namespace ikaros
+{    
+    //char *      create_jpeg(long int & size, matrix & data, float minimum=0, float maximum=1, int quality=100);
+
+    char * create_jpeg(long int & size, matrix & red, matrix & green, matrix & blue, int quality=100);
+    void destroy_jpeg(char * jpeg);
+
+    //void        decode_jpeg(float ** matrix, int sizex, int sizey, char * data, long int size);
+    //void        decode_jpeg(float ** red_matrix, float ** green_matrix, float ** blue_matrix, int sizex, int sizey, char * data, long int size);
+
+    //bool        jpeg_get_info(int & sizex, int & sizey, int & planes, char * data, long int size);
+    //void        jpeg_decode(float ** red_matrix, float ** green_matrix, float ** blue_matrix, float ** intensity_matrix, int sizex, int sizey, char * data, long int size);
+};
+
+#endif
