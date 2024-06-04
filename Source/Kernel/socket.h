e7016b204277cac57ff9937228c1e384b2e26e15
Christian Balkenius <christian@ikaros-project.org>
2024-06-04 22:55:50 +0200

    Update socket.h

--- Source/Kernel/socket.h
+++ Source/Kernel/socket.h
@@ -1,32 +1,14 @@
 //
 //	IKAROS_Socket.h		Socket utilities for the IKAROS project
 //
-//    Copyright (C) 2001-2011  Christian Balkenius
+//    Copyright (C) 2001-2024  Christian Balkenius
 //
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License
-//    along with this program; if not, write to the Free Software
-//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//    See http://www.ikaros-project.org/ for more information.
-//
-
 
 #ifndef SOCKET
 #define SOCKET
 
 #include <stddef.h>
 
-//#include "IKAROS_System.h"
 #include "utilities.h"
 #include "deprecated.h"
 
@@ -37,7 +19,7 @@
 class SocketException
 	{
 	public:
-		char	*		string;
+		char	*	string;
 		int			internal_reference;
 		
 		SocketException(char * s, int ref = 0) : string(s), internal_reference(ref)
