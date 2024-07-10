35b48ba8f486f52965c8147c68a40b460de430f9
Pierre Klintefors <45684555+pierreklintefors@users.noreply.github.com>
2024-07-10 16:16:42 +0200

    Adding Ranking input as option to choose the position output

--- Source/Modules/RobotModules/CompliantRobot/PositionSelection/PositionSelection.cc
+++ Source/Modules/RobotModules/CompliantRobot/PositionSelection/PositionSelection.cc
@@ -9,12 +9,12 @@ class PositionSelection: public Module
     matrix position_input;
     matrix position_output;
     matrix previous_input;
+    matrix inputRank;
 
 
     int difference_margin;
     bool new_goal;
 
-    
 
 
     void Init()
@@ -22,38 +22,43 @@ class PositionSelection: public Module
     
         Bind(position_input, "PositionInput");
         Bind(position_output, "PositionOutput");
+        Bind(inputRank, "InputRanking");
+
 
         previous_input.copy(position_input);
         previous_input.set_name("PreviousOutput");
+
         
         difference_margin=1;
         new_goal = false;
-        
-
 
     }
 
 
     void Tick()
     {     
-
-
-        auto [changed, changed_row] = matrix_comparison(position_input, previous_input, difference_margin);
-        
-        if(changed)
-        {   
-            std::cout << "new goal" << std::endl;
-            position_output.copy(position_input[changed_row]);
+        if(inputRank.connected() && position_input.rank()>1){
+            position_output.copy(position_input[inputRank[0]]);
         }
-        
-        // Avoid setting goal positions to zero
-        for(int i=0; i<position_output.size(); i++)
-            if(position_output[i] == 0)
-                position_output[i] = position_input(0,i);
-        
-        previous_input.copy(position_input);
+        else if (position_input.connected() && position_input.rank()>1)
+            {
+            auto [changed, changed_row] = matrix_comparison(position_input, previous_input, difference_margin);
+            
+            if(changed)
+            {   
+                std::cout << "new goal" << std::endl;
+                position_output.copy(position_input[changed_row]);
+            }
+            
+            // Avoid setting goal positions to zero
+            for(int i=0; i<position_output.size(); i++)
+                if(position_output[i] == 0)
+                    position_output[i] = position_input(0,i);
+            
+            previous_input.copy(position_input);
 
-        
+            
+        }
     }
 
     
@@ -69,7 +74,7 @@ class PositionSelection: public Module
         }
         
         return {false, -1};
-    }
+    } 
 };
 
 
