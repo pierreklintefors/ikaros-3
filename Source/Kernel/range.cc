c2d17d80a3fc55398b2b9618e9099d211f0ea819
Christian Balkenius <christian@ikaros-project.org>
2024-08-28 22:48:25 +0200

    Additional range functionality

--- Source/Kernel/range.cc
+++ Source/Kernel/range.cc
@@ -51,7 +51,46 @@ namespace ikaros
         return *this;
     }
 
+    range & 
+    range::extend(int n)
+    {
+        while(rank() < n)
+            push();
+        return *this;
+    }
 
+
+   range & 
+   range::extend(const range & r)
+   {
+        extend(r.rank());
+        for(int i=0; i<rank(); i++)
+        {
+            if(inc_[i] !=0 && inc_[i] != r.inc_[i])
+                throw std::runtime_error("Incompatible range increments");
+            a_[i] = std::min(a_[i], r.a_[i]);
+            b_[i] = std::max(b_[i], r.b_[i]);
+            inc_[i] = r.inc_[i];
+            index_[i] = inc_[i]>0 ? a_[i] : a_[i]+inc_[i]*((b_[i]-a_[i]-1)/inc_[i]);
+        }
+        return *this;
+   } 
+
+
+    range & 
+    range::fill(const range & r)
+    {
+        for(int i=0; i<rank(); i++)
+            if(empty(i))
+            {
+                a_[i] = r.a_[i];
+                b_[i] = r.b_[i];
+                inc_[i] = r.inc_[i];
+            }
+            return *this;
+    }
+
+    
     std::vector<int> range::extent()
     {
         return b_;
@@ -59,43 +98,50 @@ namespace ikaros
 
     range::range(std::string s)
     {
-        if(s.empty())
-            return;
-        if(s[0] != '[')
-            throw std::invalid_argument("Malformed range string");
-        if(s[s.size()-1] != ']')
-            throw std::invalid_argument("Malformed range string");
+        try
+        {        
+            if(s.empty())
+                return;
+            if(s[0] != '[')
+                throw std::invalid_argument("Malformed range string");
+            if(s[s.size()-1] != ']')
+                throw std::invalid_argument("Malformed range string");
 
-        std::vector<std::string> ranges = split(s.substr(1, s.size()-2), "][");
+            std::vector<std::string> ranges = split(s.substr(1, s.size()-2), "][");
 
-        for(auto & ss : ranges)
+            for(auto & ss : ranges)
+            {
+                auto & r = split(ss, ":");
+                if(r.size()==1)
+                {
+                    if(r[0].empty())
+                        push(0, 0, 0);
+                    else
+                        push(std::stoi(r[0]), std::stoi(r[0])+1, 1); // single index
+                }
+                else if(r.size()==2)
+                {
+                    int a = r[0].empty() ? 0 : std::stoi(r[0]);
+                    int b = r[1].empty() ? 0 : std::stoi(r[1]);
+                    if(a== 0 && b==0)
+                        push(0, 0, 0);
+                    else
+                        push(a, b, 1);
+                }
+                else if(r.size()==3)
+                {
+                    int a = r[0].empty() ? 0 : std::stoi(r[0]);
+                    int b = r[1].empty() ? 0 : std::stoi(r[1]);
+                    int i = r[2].empty() ? (a==0 && b==0 ? 0 : 1) : std::stoi(r[2]);
+                    push(a, b, i);
+                }
+                else
+                throw std::invalid_argument("Malformed range string");
+            }
+        }
+        catch(const std::exception& e)
         {
-            auto & r = split(ss, ":");
-            if(r.size()==1)
-            {
-                if(r[0].empty())
-                    push(0, 0, 0);
-                else
-                    push(std::stoi(r[0]), std::stoi(r[0])+1, 1); // single index
-            }
-            else if(r.size()==2)
-            {
-                int a = r[0].empty() ? 0 : std::stoi(r[0]);
-                int b = r[1].empty() ? 0 : std::stoi(r[1]);
-                if(a== 0 && b==0)
-                    push(0, 0, 0);
-                else
-                    push(a, b, 1);
-            }
-            else if(r.size()==3)
-            {
-                int a = r[0].empty() ? 0 : std::stoi(r[0]);
-                int b = r[1].empty() ? 0 : std::stoi(r[1]);
-                int i = r[2].empty() ? (a==0 && b==0 ? 0 : 1) : std::stoi(r[2]);
-                push(a, b, i);
-            }
-            else
-               throw std::invalid_argument("Malformed range string");
+            throw std::invalid_argument("Malformed range string");
         }
     }
 
@@ -115,26 +161,30 @@ namespace ikaros
         return index();
     }
 
-    void range::reset(int d)
+    range & 
+    range::reset(int d)
     {
         if(d>=index_.size())
-            return;
+            return *this;
         if(inc_[d] == 0)
-            return;
+            return *this;
         index_[d] = inc_[d]>0 ? a_[d] : a_[d]+inc_[d]*((b_[d]-a_[d]-1)/inc_[d]);
+        return *this;
     }
 
 
-    void range::clear()
+    range & 
+    range::clear()
     {
         inc_.clear();
         a_.clear();
         b_.clear();
         index_.clear();
+        return *this;
     }
 
 
-    bool range::more(int d)
+    bool range::more(int d) const
     {
         if(inc_.empty())
             return false;
@@ -150,6 +200,17 @@ namespace ikaros
             r.b_[d] -= r.a_[d];
             r.a_[d] = 0;
         }
+        return r;
+    }
+
+
+    range 
+    range::strip()
+    {
+        range r;
+        for(int i=0; i<index_.size(); i++)
+            if(b_[i]-a_[i] > 1)
+                r.push(a_[i], b_[i], inc_[i]);
         return r;
     }
 
@@ -178,12 +239,17 @@ namespace ikaros
 
 
 
-    bool range::empty()
+    bool range::empty() const
     {
         return a_.size() == 0;
     }
 
 
+
+    bool range::empty(int d) const
+    {
+        return a_[d] == b_[d] || inc_[d] == 0;
+    }
 
     range::operator std::vector<int> &() { return index(); };
 
@@ -196,14 +262,16 @@ namespace ikaros
         return *this;
     }
 
-    void range::operator=(const std::vector<int> & v)
+    void 
+        range::operator=(const std::vector<int> & v)
     {
         clear();
         for(auto & i : v)
             push(0, i);
     }
 
-    void operator|=(range & r, range & s)
+    void 
+        operator|=(range & r, range & s)
     {
         if(r.index_.size()==0)
         {
@@ -222,11 +290,31 @@ namespace ikaros
         }
     }
 
-    void range::print()
+    void
+    range::print(std::string name)
     {
-        std::cout << *this << std::endl;
+        std::cout << name << ": " << std::string(*this) << std::endl;
     }
-    void range::print_index()
+
+
+    void 
+    range::info(std::string name)
+    {
+        std::string tab;
+        if(!name.empty())
+        {
+            std::cout << name << ": " << std::endl;
+            tab = "\t";
+        }
+        std::cout << tab << "rank: " << rank() << std::endl;
+        std::cout << tab << "size: " << size() << std::endl;
+        std::cout << tab << "value: "  << std::string(*this) << std::endl;
+        std::cout << tab << std::endl;
+    }
+
+
+    void 
+    range::print_index()
     {
         for(auto x: index_)
             std::cout << x << " ";
@@ -268,7 +356,8 @@ namespace ikaros
     }
 
 
-    std::string range::curly()
+    std::string 
+    range::curly()
     {
         if(index_.empty())
             return "";
@@ -305,42 +394,55 @@ namespace ikaros
         return s;   
     }
 
-
-
-    bool operator==(range & a, range & b)
+    bool 
+    operator==(range & a, range & b)
     {
         return a.index_==b.index_ && a.a_==b.a_ && a.b_==b.b_ && a.inc_==b.inc_;
     }
     
-    bool operator!=(range & a, range & b)
+    bool 
+    operator!=(range & a, range & b)
     {
         return !(a==b);
     }
 
-    bool operator<(range & a, range & b)
+    bool 
+    operator<=(range & a, range & b)
     {
+        throw std::runtime_error("operator <= not implemented");
         return false;
     }
 
-    int range::size() // FIXME: Change name and use size() for dimenions.
+    int
+    range::size(int d)
+    {
+        if(inc_[d]==0)
+            return 0; // FIXME: should this throw an expection?
+        else
+            return (b_[d]-a_[d])/inc_[d];
+    }
+
+    int 
+    range::size()
     {
         if(index_.size() == 0)
             return 0;
 
         int s=1;
-        for(int d=0; d< index_.size(); d++)
-            s *= (b_[d]-a_[d]);
+        for(int d=0; d<index_.size(); d++)
+            s *= size(d);
       return s;
     }
 
     std::ostream& operator<<(std::ostream& os, const range & x);
     
-    int range::rank()
+    int range::rank() const
     {
         return index_.size();
     }
 
-    std::ostream& operator<<(std::ostream& os, const range & x)
+    std::ostream& 
+    operator<<(std::ostream& os, const range & x)
     {
         std::string sep;
         std::cout << "(";
@@ -352,4 +454,12 @@ namespace ikaros
         std::cout << ")";
         return os;
     }
+
+    range::range(std::initializer_list<std::tuple<int, int, int>> ranges) 
+    {
+        for (const auto& [a, b, inc] : ranges) {
+            push(a, b, inc);
+        }
+    }
+
 }
