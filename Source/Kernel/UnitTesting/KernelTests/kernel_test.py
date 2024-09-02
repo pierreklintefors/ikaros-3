cd584714e12cf0120898c330d4f8c078635deccd
Christian Balkenius <christian@ikaros-project.org>
2024-09-02 20:33:40 +0200

    Update kernel_test.py

--- Source/Kernel/UnitTesting/KernelTests/kernel_test.py
+++ Source/Kernel/UnitTesting/KernelTests/kernel_test.py
@@ -16,32 +16,26 @@ red = "\033[31m"
 reset = "\033[0m"
 
 def get_description(file_path):
-    tree = ET.parse(file_path)
-    root = tree.getroot()
-    value = root.get("description")
+    value = ET.parse(file_path).getroot().get("description")
     return '' if value is None else value+" – "
 
 print(f"\n{bold}Running Ikaros Unit Tests{reset}\n")
 errors = 0
-file_count = 0
 current_directory = Path(__file__).resolve().parent
 ikaros_binary = current_directory / "../../../../Bin/ikaros_d"
 
 test_files = [item for item in current_directory.iterdir() if item.name.startswith("test") and item.suffix.lower() == '.ikg']
-
-test_files.sort()
-for item in test_files:
-    if item.name.startswith("test") and item.suffix.lower() == '.ikg':
-        file_count+=1
+for item in sorted(test_files):
         result = subprocess.run( f"{ikaros_binary} -s0 -b {item}", shell=True, text=True, capture_output=True)
         if result.returncode == 0:
             print(f"[  OK  ]  {get_description(item)}{item.name}{reset}")
         else:
-            print(f"{red}{bold}[ FAIL ]  {get_description(item)}{item.name}{reset}")
+            print(f"{red}{bold}[ FAIL ]  {get_description(item)}{item.name}{reset} ({result.stdout.strip().split('\n')[-1]})")
             errors += 1
 
 if errors > 0:
     print(f"\n{red}{bold}***** Ikaros failed {errors} tests *****{reset}\n")
 else:
-    print(f"\nIkaros passed all {file_count} tests\n")
+    print(f"\nIkaros passed all {len(test_files)} tests\n")
 
+ 
